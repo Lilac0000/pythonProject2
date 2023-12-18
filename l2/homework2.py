cb7b066576c56284ffa0ca3e3d1c65bcd9506c45
Lilac0000 <nataliashmyreva@gmail.com>
2023-12-18 16:15:26 -0500

    #2nd commit

--- l2/homework2.py
+++ l2/homework2.py
@@ -1,1 +1,10 @@
-print('hello')
+x = input('Enter a 4 digit-number: ')
+digit_1 = int(x) // 1000
+digit_2 = int(x) % 1000 // 100
+digit_3 = int(x) % 100 // 10
+digit_4 = int(x) % 10
+print (digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
+

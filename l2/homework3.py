478f8d2d2846ceb1fbf29a36735ab459067beaab
Lilac0000 <nataliashmyreva@gmail.com>
2023-12-19 09:49:21 -0500

    #4th commit

--- l2/homework3.py
+++ l2/homework3.py
@@ -4,8 +4,7 @@ digit_2 = int(x) % 10000 // 1000
 digit_3 = int(x) % 1000 // 100
 digit_4 = int(x) % 100 // 10
 digit_5 = int(x) % 10
-print(digit_5)
-print(digit_4)
-print(digit_3)
-print(digit_2)
-print(digit_1)
+digit_6 = digit_5 * 10000 + digit_4 * 1000 + digit_3 * 100 + digit_2 * 10 + digit_1 * 1
+print(digit_6)
+
+

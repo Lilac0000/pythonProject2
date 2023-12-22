7ee16589e3622f8a8f435f30cf6e06747345a953
Lilac0000 <nataliashmyreva@gmail.com>
2023-12-22 15:50:33 -0500

    #Added condition for 0 divider

---
+++ l2/homework6.py
@@ -0,0 +1,29 @@
+
+lst = [12, 3, 4, 10]
+if len(lst) > 1:
+    a = (lst.pop())
+    lst = [a] + lst
+print(lst)
+
+lst = [1]
+if len(lst) > 1:
+    a = (lst.pop())
+    lst = [a] + lst
+print(lst)
+
+lst = []
+if len(lst) > 1:
+    a = (lst.pop())
+    lst = [a] + lst
+print(lst)
+
+lst = [12, 3, 4, 10, 8]
+if len(lst) > 1:
+    a = (lst.pop())
+    lst = [a] + lst
+print(lst)
+
+
+
+
+

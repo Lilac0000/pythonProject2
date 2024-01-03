ea9fd8894a619fa99143a3ce8956a08184f691f4
Lilac0000 <nataliashmyreva@gmail.com>
2024-01-03 14:26:46 -0500

    #1st commit

---
+++ l2/homework.11.py
@@ -0,0 +1,26 @@
+while True:
+    num1 = float(input('Enter first number: '))
+    operator = input('Enter operator (+,-,*,/): ')
+    num2 = float(input('Enter second number: '))
+
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Invalid Operation"
+    else:
+        result = "Invalid Operation"
+
+
+    print(result)
+
+    user_input = input('Do you want to continue? "yes" or "y":')
+    if user_input not in ('yes', 'y'):
+        break
+

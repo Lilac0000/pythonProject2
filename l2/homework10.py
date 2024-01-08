b574fe326349d31ea1f0766691b5166aefcd3751
Lilac0000 <nataliashmyreva@gmail.com>
2024-01-08 09:32:24 -0500

    #1st

---
+++ l2/homework10.py
@@ -0,0 +1,21 @@
+import string
+import keyword
+
+name = input('Enter a string: ')
+
+
+if name[0].isdigit():
+    print(False)
+elif name.isdigit():
+    print(False)
+elif name in keyword.kwlist:
+    print(False)
+elif ' ' in name:
+    print(False)
+else:
+    for n in name:
+        if n in string.punctuation and n != '_':
+            print(False)
+            break
+    else:
+        print(True)
